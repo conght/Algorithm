31cca116cf71cf7aa240c9830b15dfefe80f12ae
conghanting <nitingge@126.com>
2017-11-29 17:31:24 +0800

    AC 188

--- leetcode-188.cc
+++ leetcode-188.cc
@@ -15,73 +15,55 @@ public:
         
         int n = prices.size();
         
-        
-        int max_sum = 0;
-
-        int *dpl = new int[n];
-        int *dpr = new int[n];
-
-        for(int i=0;i<n;i++) {
-            dpl[i] = dpr[i] = 0;
+        if (k > n) {
+            return _maxProfit(prices);
         }
 
-        // from left -> right
-        int _min1 = prices[0];
-        int _maxprofit1 = 0;
-        for (int i=1;i<n;i++) {
-            int tmp = prices[i] - _min1;
-            _maxprofit1 = _maxprofit1 > tmp ? _maxprofit1 : tmp;
-            if (prices[i] < _min1) {
-                _min1 = prices[i];
+        int **localProfit = new int *[n];  
+        int **globalProfit = new int *[n];
+
+        for (int i=0;i<n;i++) {
+            localProfit[i] = new int [k+1];
+            globalProfit[i] = new int [k+1];
+        }
+
+        for (int i=0;i<n;i++)
+            for (int j=0;j<k+1;j++)
+                localProfit[i][j] = globalProfit[i][j] = 0;
+ 
+        for (int i = 1; i < n; ++i) {
+            int diff = prices[i]-prices[i-1];
+            for (int j = k; j > 0; j--) {
+                localProfit[i][j] = max(globalProfit[i-1][j-1]+max(diff, 0), localProfit[i-1][j] + diff);
+                globalProfit[i][j] = max(globalProfit[i-1][j], localProfit[i][j]);
             }
-            dpl[i] = _maxprofit1;
         }
 
-        // from right -> left
+        int ret = globalProfit[n-1][k];
 
-        int _max2 = prices[n-1];
-        int _maxprofit2 = 0;
-        for (int i=n-2;i>=0;i--) {
-            int tmp = _max2 - prices[i];
-            _maxprofit2 = _maxprofit2 > tmp ? _maxprofit2 : tmp;
-            if (prices[i] > _max2) {
-                _max2 = prices[i];
-            }
-            dpr[i] = _maxprofit2;
-        }
-        
-        for (int k=1;k<n-2;k++) {
-            int tmp = dpl[k] + dpr[k+1];
-            if (tmp > max_sum)
-                max_sum = tmp;
-        }
-        // transaction one time
-        max_sum = max(max_sum, _maxProfit(prices, 0, n-1));
-        
-        delete dpl;
-        delete dpr;
-        return max_sum;
+        delete [] localProfit;
+        delete [] globalProfit;
+
+        return ret;
     }
     
-    int _maxProfit(vector<int>& prices, int start, int end) {
-        int maxprofit = 0;
-        int _min=prices[start];
-        for (int i=start+1;i<end+1;i++) {
-            if (_min < prices[i]) {
-                maxprofit = (prices[i] - _min) > maxprofit ? prices[i] - _min : maxprofit;
-            }
-            if (_min > prices[i])
-                _min = prices[i];
+    int _maxProfit(vector<int>& prices) {
+        int result = 0;
+        int n = prices.size();
+        for(int i = 1; i < n; i ++){
+            if(prices[i] > prices[i-1])
+                result += (prices[i] - prices[i-1]);
         }
-        return maxprofit;
+        return result;
     }
 };
 
 int main() {
     Solution* s = new Solution();
-    std::vector<int> v = {1,2,4,2,5,7,2,4,9};
+    int v0[6] = {6,1,3,2,5,7};
+    std::vector<int> v(v0,v0+6);
     
-    cout << s->maxProfit(v) << endl;
+    cout << s->maxProfit(2, v) << endl;
     delete s;
     return 0;
 }
