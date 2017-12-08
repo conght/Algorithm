d82c99efc501c2249a05df5e40e1bdb0ddc885a0
conghanting <nitingge@126.com>
2017-12-09 01:21:51 +0800

    cm

--- leetcode-139.cc
+++ leetcode-139.cc
@@ -9,30 +9,32 @@ using namespace std;
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-    	vector<bool> dp(s.length()+1, false);
-    	map<string, bool> dict;
-    	for (auto iter = wordDict.begin();iter != wordDict.end();iter++)
-    		dict.insert(make_pair(*iter, true));
-    	dp[0] = true;
-    	for (int i=0;i < s.length();i++) {
-    		if (dict.find(s.substr(0, i+1)) != dict.end()) {
-    			dp[i+1] = true;
-    			continue;
-    		} 
-
-			for (int j=0;j<i;j++) {
-				if (dp[j] && dict.find(s.substr(j+1, i)) != dict.end())
-					dp[i] = true;
-					continue;
-			}
-    	}
-    	return dp[s.length()+1];
+        vector<bool> dp(s.length()+1, false);
+        map<string, bool> dict;
+        for (auto iter = wordDict.begin();iter != wordDict.end();iter++)
+            dict.insert(make_pair(*iter, true));
+        dp[0] = true;
+        for (int i=1;i < s.length() + 1;i++) {
+            if (dict.find(s.substr(0, i)) != dict.end()) {
+                dp[i] = true;
+                continue;
+            }
+            for (int j=1;j<i;j++) {
+                string tmp =s.substr(j, i-j);
+                //cout<<tmp<<endl;
+                if (dp[j] && dict.find(s.substr(j, i-j)) != dict.end()) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
     }
 };
 
 int main() {
     Solution* s = new Solution();
-    string a = "leedcode";
+    string a = "leetcode";
     std::vector<string> v;
     v.push_back("leet");
     v.push_back("code");
