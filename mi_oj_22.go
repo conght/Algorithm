5340fdd3bcb63e466db8ec7c2e459453ccf0e1e3
conght <nitingge@126.com>
2018-03-14 20:55:59 +0800

    cm

--- mi_oj_22.go
+++ mi_oj_22.go
@@ -11,11 +11,31 @@ func solution(line string) string {
     // 在此处理单行数据
     n,_ := strconv.Atoi(line)
 
+    // curr:=1
+    // plus:=true
+    // for i:=2;i<=n;i++ {
+    //     if (plus) {
+    //         curr ++
+    //     } else {
+    //         curr --
+    //     }
+            
+    //     if (curr == 10) {
+    //         curr = 8
+    //         plus = false
+    //     } 
+    //     if (curr == 0) {
+    //         curr = 2
+    //         plus = true
+    //     }
+        
+    // }
+    // return strconv.Itoa(line)
     x:= 1 
     for {
         sum := (1+x)*x/2
         if (sum >= n) {
-            x--
+            x-=1
             break
         }
         x++
@@ -27,23 +47,33 @@ func solution(line string) string {
     //fmt.Println(n)
 
 
-    if (n <= 9) {
-        return strconv.Itoa(n)
-    } 
-    if (n <= 16) {
-        return strconv.Itoa(17-n)
-    }
+    // if (n <= 9) {
+    //     return strconv.Itoa(n)
+    // } 
+    // if (n <= 16) {
+    //     return strconv.Itoa(17-n)
+    // }
 
+    ret := ""
     if (n/16 > 0) {
-        n = n%16 
+        n = n%16
+        
     }
-
-    if (n/9 > 0) {
+    if (n == 0) {
+        return "2"
+    }
+    if (n <= 9) {
+        ret= strconv.Itoa(n)
+    } else {
+        ret= strconv.Itoa(18-n)
+    }
+    return ret
+    /*if (n/9 > 0) {
         n = n%9
         return strconv.Itoa(9-n)
     } else {
         return strconv.Itoa(n)
-    }
+    }*/
     // 返回处理后的结果
     //return
 }
