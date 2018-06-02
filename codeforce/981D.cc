d1c1bfeb92a5912cfd4c45c741b46c6a89ebdca8
conghanting <nitingge@126.com>
2018-06-02 14:40:32 +0800

    cm

--- codeforce/981D.cc
+++ codeforce/981D.cc
@@ -4,7 +4,9 @@
 
 using namespace std;
 
-long long d[51][51];
+unsigned long long d[51][51];
+unsigned long long s[51][51];
+unsigned long long a[51];
 
 long long mysum(std::vector<long long>& a, int start, int end)
 {
@@ -56,7 +58,56 @@ void solution()
 
 }
 
+typedef unsigned long long ull;
+
+ull mysum2(int start, int end)
+{
+    ull ans = 0;
+    for (int i=start;i<=end;i++)
+        ans += a[i];
+    return ans;
+}
+
+void solution2()
+{
+    int n,k;
+    cin >> n;
+    cin >>k;
+
+    //std::vector<ull> a(n+1);
+
+    for (int i=1;i<=n;i++)
+    {
+        cin >> a[i];
+    }
+
+    d[1][0] = a[1];
+
+    //for (int i=1;i<=n;i++)
+    //    d[i][0] = mysum2(a, 1, i);
+
+    for (int i=2;i<=n;i++)
+    {
+        d[i][0] = mysum2(1, i);
+        for(int j=1;j<i;j++)
+        {
+            for (int m=0;m<k-1 && m<=j-1;m++)
+            {
+                d[i][m+1] = max(d[i][m+1], d[j][m]& mysum2(j+1, i));
+            }
+        }
+    }
+    
+    for (int i=1;i<=n;i++) 
+    {
+        for(int j=0;j<=k;j++)
+            cout << d[i][j] << " ";
+        cout << endl; 
+    }
+
+}
+
 int main()
 {
-    solution();
+    solution2();
 }
