0d451d186b1d8cfbc8be40116b007c3da3f06827
conght <nitingge@126.com>
2018-09-12 23:41:32 +0800

    cm

--- leetcode/leetcode-03.cc
+++ leetcode/leetcode-03.cc
@@ -5,38 +5,29 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int ans = 1;
+        int ans = 0;
 
         if (s.length() == 0)
             return 0;
-        set<char> chars;
-        for (int i=0;i<s.length();i++) {
-            chars.insert(s[i]);
-        }
 
-        int len = chars.size();
-        vector<vector<bool> > d(s.length()+1, vector<bool>(s.length()+1));
-        /*bool ** d = new bool * [s.length()+1];
-        for (int i=0;i<s.length()+1;i++) {
-            d[i] = new bool[s.length()+1];
-        }*/
-
-        for (int i=0;i<s.length()+1;i++) {
-            d[i][i] = true;
-        }
-
-        for (int i=1;i<len;i++) {
-            for (int j=0;j<s.length()-i;j++) {
-                d[j][j+i] = s[j] == s[j+i] ? false : true;
-                d[j][j+i] = (d[j][j+i-1] && d[j+1][j+i] && d[j][j+i]);
-
-                if (d[j][j+i] && (i+1) > ans)
-                    ans = (i+1);
+        set<char> sets;
+        int index[256] = {-1};
+        int i = 0;
+        while (i < s.length()) {
+            int j = i;
+            while (j < s.length()) {
+                sets.insert(s[j]);
+                if ((j - i + 1) == sets.size()) {
+                    ans = ans > sets.size() ? ans : sets.size();
+                    index[s[j]] = j;
+                    ++j;
+                } else {
+                    i = index[s[j]] + 1;
+                    sets.clear();
+                    break;
+                }
             }
         }
-        /*for(int j=0;j<s.length()+1;j++)
-            delete[] d[j];
-        delete[] d;*/
         return ans;
     }
 };
