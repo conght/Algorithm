8f27c25067da04d965c684c58cf991d386ad5d56
conght <nitingge@126.com>
2018-10-03 23:47:18 +0800

    cm

---
+++ leetcode/leetcode-117.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+struct TreeLinkNode {
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+
+    }
+};
