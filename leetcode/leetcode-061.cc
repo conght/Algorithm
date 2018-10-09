20211b5ab7069700de29ef3bbe5f59d910ba0a0e
conght <nitingge@126.com>
2018-10-09 23:58:16 +0800

    cm

---
+++ leetcode/leetcode-061.cc
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+
+        int ans = head;
+
+        if (NULL == head) return ans;
+
+        int length = 0;
+        while (head) {
+            ++length;
+            head = head->next;
+        }
+
+        k = k%length;
+
+        if (0 == k) return ans;
+
+        k = length - k;
+        ListNode* p,q;
+        head = p = q = ans;
+        for (int i=0; i<k-1; i++) p = p->next;
+        q = p->next;
+        q->next = head;
+        p->next = NULL;
+        ans = q;
+
+        return ans;
+    }
+};
